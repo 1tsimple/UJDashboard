a4dbfafbf66cf7299deebfca309ae8260cb7cfb3
1tsimple <s.ardamermer@gmail.com>
2023-04-17 11:26:52 +0300

    close graph functionality implemented

--- assets/scripts/graph.js
+++ assets/scripts/graph.js
@@ -1,5 +1,7 @@
 document.addEventListener("click", (event) => {
   if (event.target.classList.contains("clickable-close-button-dummy")) {
-    event.target.parentElement.parentElement.parentElement.parentElement.remove()
+    let graph_container = event.target.parentElement.parentElement.parentElement.parentElement
+    var container = graph_container.parentElement
+    graph_container.remove()
   }
 });
