55b077fb7af8b83943c88a3f35f13370b23e3dba
1tsimple <s.ardamermer@gmail.com>
2023-04-19 20:17:52 +0300

    added functionality to toggle collapsible with apply filter button

--- assets/scripts/graph.js
+++ assets/scripts/graph.js
@@ -1,13 +1,20 @@
 document.addEventListener("click", (event) => {
   if (event.target.classList.contains("clickable-close-button-dummy")) {
     let graph_container = event.target.parentElement.parentElement.parentElement.parentElement;
-    var container = graph_container.parentElement;
     graph_container.remove();
-  }
-  if (event.target.type === "button" && event.target.classList.contains("filter-button")) {
+  } else if (event.target.type === "button" && event.target.classList.contains("filter-button")) {
     let collapsible = event.target.parentElement.parentElement.getElementsByClassName("filter-collapse")[0];
     collapsible.classList.toggle("show");
+  } else if ((event.target.type === "submit" && event.target.classList.contains("filter-apply-button")) || (event.target.parentElement.type === "submit" && event.target.parentElement.classList.contains("filter-apply-button"))) {
+    let target
+    if (event.target.type === "submit" && event.target.classList.contains("filter-apply-button")) {
+      target = event.target.parentElement.parentElement;
+    } else {
+      target = event.target.parentElement.parentElement.parentElement;
+    }
+    let collapsible = event.target.parentElement.parentElement.getElementsByClassName("filter-collapse")[0];
+    if (collapsible.classList.contains("show")) {
+      collapsible.classList.remove("show")
+    }
   }
-  console.log(event.target)
-  /* TODO: apply button basılınca collapsible'ı kapat */
 });
